0eba76708ab44659326af4d5685d6f43256e06d3
Nguyen Vu Hoa Binh <nguyenvuhoabinh.ai@gmail.com>
2025-03-25 08:59:06 +0700

    remove identity-service

--- agentflow/services/identity-service/src/main/java/viettel/dac/identityservice/IdentityServiceApplication.java
+++ agentflow/services/identity-service/src/main/java/viettel/dac/identityservice/IdentityServiceApplication.java
@@ -2,13 +2,8 @@ package viettel.dac.identityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("viettel.dac.identityservice.model")
-@EnableJpaRepositories("viettel.dac.identityservice.repository")
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
